322d824b34532fe41d6756300348732805f17ec5
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-06-04 15:36:28 -0600

    linted grader.js with standard and nodemon

--- grader.js
+++ grader.js
@@ -1,56 +1,53 @@
 module.exports = {
-  letterGrader: function(num) {
-  if (num >=90 && num <= 100) {
-    return 'A';
+  letterGrader: function (num) {
+    if (num >= 90 && num <= 100) {
+      return 'A'
+  } else if (num >= 80 && num <= 89) {
+    return 'B'
+  } else if (num >= 70 && num <= 79) {
+    return 'C'
+  } else if (num >= 60 && num <= 69) {
+    return 'D'
+  } else if (num >= 0 && num <= 59) {
+    return 'F'
+  } else {
+    return 'not possible'
   }
-  else if (num >=80 && num <= 89) {
-      return 'B';
-  }
-  else if (num >=70 && num <= 79) {
-      return 'C';
-  }
-  else if (num >=60 && num <= 69) {
-      return 'D';
-  }
-  else if (num >=0 && num <= 59) {
-      return 'F';
-  }
-  else {
-    return 'not possible';
+  },
+
+  averageScore: function (arr) {
+    var result = 0
+    for (var i = 0; i < arr.length; i++) {
+      result += arr[i] / arr.length
+    }
+    return result
+  },
+
+  medianScore: function (arr) {
+    arr.sort()
+    var half = Math.floor(arr.length / 2)
+    if (arr.length % 2) {
+      return arr[half]
+    } else {
+      return (arr[half - 1] + arr[half]) / 2
     }
   },
 
-  averageScore: function(arr) {
-    var result = 0;
-    for(var i = 0; i < arr.length; i++){
-        result += arr[i]/arr.length;
-    }
-    return result;
-  },
+  modeScore: function (arr) {
+    var mode = {}
+    var max = 0
+    var count = 0
 
-  medianScore: function(arr) {
-      arr.sort();
-      var half = Math.floor(arr.length/2);
-      if(arr.length % 2)
-          return arr[half];
-      else
-          return (arr[half-1] + arr[half]) / 2.0;
-  },
-
-  modeScore: function(arr) {
-    var mode = {};
-    var max = 0;
-    var count = 0;
-
-    arr.forEach(function(e) {
-      if (mode[e]) { mode[e]++; }
-        else { mode[e] = 1; }
-
-        if (count<mode[e]) {
-            max = e;
-            count = mode[e];
-            }
-        });
-        return max;
-    }
-  };
+    arr.forEach(function (e) {
+      if (mode[e]) {
+        mode[e]++
+      } else {
+        mode[e] = 1
+      } if (count < mode[e]) {
+        max = e
+        count = mode[e]
+      }
+    })
+    return max
+  }
+  }
